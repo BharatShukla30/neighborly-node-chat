04cd59ffd3135ca06638d7726304cf491de4ca51
Bharat Shukla <bharatsagii30@gmail.com>
2024-04-21 00:06:34 +0530

    implemented logging

--- node-backend/app.js
+++ node-backend/app.js
@@ -12,58 +12,87 @@ const {
   upVote,
   downVote,
 } = require("./controllers/chatController");
+const { activityLogger, errorLogger } = require("./utils/logger");
 
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
   },
 });
+
+// Connect to the database
 connectDatabase();
+
 const PORT = process.env.PORT || 3001;
 
 io.on("connection", (socket) => {
-  //------------------------------------------------ To activate a user in a specific group chat -------------------------------------------
+  activityLogger.info(`New connection: ${socket.id}`);
+
   socket.on("join-room", ({ username, group_id }) => {
-    joinRoom(socket, username, group_id);
+    try {
+      joinRoom(socket, username, group_id);
+      activityLogger.info(`User ${username} joined room ${group_id}`);
+    } catch (error) {
+      errorLogger.error("Error joining room", error);
+    }
   });
-  //-----------------------------------------------------------------------------------------------------------------------------------------
 
-  //---------------------------------------------- To deactivate a user from a specific group chat ------------------------------------------
   socket.on("leave-room", ({ username, group_id }) => {
-    leaveRoom(socket, username, group_id);
+    try {
+      leaveRoom(socket, username, group_id);
+      activityLogger.info(`User ${username} left room ${group_id}`);
+    } catch (error) {
+      errorLogger.error("Error leaving room", error);
+    }
   });
-  //-----------------------------------------------------------------------------------------------------------------------------------------
 
-  //---------------------------------------------- Sending message --------------------------------------------------------------------------
   socket.on(
     "send-message",
     async ({ group_id, senderName, senderPhoto, msg, sent_at }) => {
-      await sendMessage(group_id, senderPhoto, senderName, msg, sent_at);
-      socket
-        .to(group_id)
-        .emit("receive_message", {
+      try {
+        await sendMessage(group_id, senderPhoto, senderName, msg, sent_at);
+        socket.to(group_id).emit("receive_message", {
           group_id: group_id,
           senderName: senderName,
           msg: msg,
           sent_at: sent_at,
         });
+        activityLogger.info(
+          `Message sent in room ${group_id} by ${senderName}`
+        );
+      } catch (error) {
+        errorLogger.error("Error sending message", error);
+      }
     }
   );
-  //-------------------------------------------------------------------------------------------------------------------------------------------
 
-  //--------------------------------------------------- UP-Vote -------------------------------------------------------------------------------
   socket.on("up-vote", async ({ msg_id }) => {
-    await upVote(msg_id);
+    try {
+      await upVote(msg_id);
+      activityLogger.info(`Message ${msg_id} up-voted`);
+    } catch (error) {
+      errorLogger.error("Error up-voting message", error);
+    }
   });
-  //-------------------------------------------------------------------------------------------------------------------------------------------
 
-  //------------------------------------------------- Down-Vote -------------------------------------------------------------------------------
   socket.on("down-vote", async ({ msg_id }) => {
-    await downVote(msg_id);
+    try {
+      await downVote(msg_id);
+      activityLogger.info(`Message ${msg_id} down-voted`);
+    } catch (error) {
+      errorLogger.error("Error down-voting message", error);
+    }
   });
-  //-------------------------------------------------------------------------------------------------------------------------------------------
+
+  socket.on("error", (error) => {
+    errorLogger.error("Socket.IO error", error);
+  });
+
+  socket.on("disconnect", (reason) => {
+    activityLogger.info(`Socket ${socket.id} disconnected: ${reason}`);
+  });
 });
 
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  activityLogger.info(`Server listening on port ${PORT}`);
 });
